1a4b4c17181749a55ac8765c000e2c791e0e7514
Derrek Meno <DERREKWHO15@gmail.com>
2020-02-05 21:10:45 -0800

    did the 3 tasks

---
+++ script.js
@@ -0,0 +1,42 @@
+// 1. Write a function min that takes two numbers and returns the smallest one of the pair.
+// Examples:
+// console.log(min(0, 10)); // will output 0.
+// console.log(min(0, -10)); // will output -10
+
+const min = function(number1, number2) {
+  if (number1 <= number2) {
+    return number1;
+  } else {
+    return number2;
+  }
+};
+console.log(min(0, 10));
+console.log(min(0, -10));
+// 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
+// Example Output:
+// console.log(calculateCircumference(5)); // will output 31.4
+
+const calculateCircumference = function(radius) {
+  const pi = 3.1415;
+  let result = 2 * pi * radius;
+  return result;
+};
+console.log(calculateCircumference(5));
+// 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
+// Example Output:
+// console.log(searchString(“s”, “mississippi”)); // will output 4
+// console.log(searchString(“q”, “mississippi”)); // will output 0
+
+let searchString = function(letter, word) {
+  let lettersFound = 0;
+
+  for (let i = 0; i <= word.length - 1; i++) {
+    if (word[i] == letter) {
+      lettersFound++;
+    }
+  }
+  return lettersFound
+};
+
+console.log(searchString("s","mississippi"));
+console.log(searchString("q","mississippi"));
